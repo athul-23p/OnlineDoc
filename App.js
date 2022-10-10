774921eac0c7c84d1ff03c84e1e7a9ac70b7bef5
athul23p <athul238p@gmail.com>
2022-10-10 13:09:57 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+
+const App = () => {
+  return <NavigationContainer></NavigationContainer>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
